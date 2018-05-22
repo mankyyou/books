3be43db57750474c29b5efb59fd488f1caec0bcc
Duc Dang-Phuc <ducdp@administrator.local.elarion.com>
2018-05-22 09:34:40 +0700

    B

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,16 @@
 class Product < ApplicationRecord
+    
+    validates :title,
+    :presence => {},    
+    :length=> {:minimum=>5}
+    validates :description,
+    :presence => {},
+    :length=> {:maximum=>200}
+    validates :image_url,format: {
+        with:
+        %r{\.(gif|jpg|png)\Z}i,
+        message: 'must be a URL for GIF, JPG or PNG image.'
+        }
+    validates :price,
+    :length=>{:in=>1..10}
 end
