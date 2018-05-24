118db15fb26c4e8280b8ecc7ba85ee3ff6c11a60
Duc Dang-Phuc <ducdp@administrator.local.elarion.com>
2018-05-24 15:37:16 +0700

    Finish F(2/2)

--- Gemfile
+++ Gemfile
@@ -1,8 +1,8 @@
 source 'https://rubygems.org'
 git_source(:github) { |repo| "https://github.com/#{repo}.git" }
-
+gem 'jquery-rails'
 ruby '2.4.1'
-
+gem 'jquery-ui-rails'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.2.0'
 # Use sqlite3 as the database for Active Record
--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -35,7 +35,8 @@ class LineItemsController < ApplicationController
     
     respond_to do |format|
       if @line_item.save
-        format.html { redirect_to store_index_url }
+        format.js{ @current_item = @line_item }
+        format.html { redirect_to store_index_url }   
 format.json { render :show,
 status: :created, location: @line_item }
       else
